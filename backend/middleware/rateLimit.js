76a9cbc7dac96612e5ae7f6a6ab13c2275520780
Baratin-Pro <baratin.pro@gmail.com>
2021-06-25 18:45:01 +0200

    :sparkles: ADD rateLimit

---
+++ backend/middleware/rateLimit.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const rateLimit = require("express-rate-limit");
+
+exports.limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 2,
+});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user.js");
+const rateLimit = require("../middleware/rateLimit.js");
 
 router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login", rateLimit.limiter, userCtrl.login);
 
 module.exports = router;
