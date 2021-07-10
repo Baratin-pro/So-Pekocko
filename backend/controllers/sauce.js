dbea400fb0fe623dde6a78caf027cea3b3a0ff30
Baratin-Pro <baratin.pro@gmail.com>
2021-07-10 09:53:28 +0200

    :sparkles: ADD modifySauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -3,7 +3,6 @@
 const Sauce = require("../models/Sauce.js");
 const sauceSchema = require("../middleware/sauceSchema.js");
 const fs = require("fs");
-const { number } = require("joi");
 
 //--------------------------------------------------------------------
 
@@ -56,4 +55,51 @@ exports.getOneSauce = (req, res, next) => {
     .catch((error) => {
       res.status(400).json({ error });
     });
+};
+
+//--------------------------------------------------------------------
+
+exports.modifySauce = async (req, res, next) => {
+  try {
+    let isValidJoi = NaN;
+
+    if (req.body.sauce) {
+      isValidJoi = await sauceSchema.validateAsync(JSON.parse(req.body.sauce));
+    } else {
+      isValidJoi = await sauceSchema.validateAsync(req.body);
+    }
+
+    if (!isValidJoi) {
+      return res.status(400).json("Données envoyées incorrecte ! ");
+    } else {
+      const sauceId = String(req.params.id);
+      Sauce.findOne({ _id: sauceId }).then((sauceFind) => {
+        if (req.file) {
+          const filename = sauceFind.imageUrl.split("/images/")[1];
+          fs.unlink(`images/${filename}`, (err) => {
+            if (err) {
+              return console.log(err);
+            } else {
+              console.log("image supprimée !");
+            }
+          });
+        }
+
+        const sauceObject = req.file
+          ? {
+              ...JSON.parse(req.body.sauce),
+              imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                req.file.filename
+              }`,
+            }
+          : { ...req.body };
+
+        Sauce.updateOne({ _id: sauceId }, { ...sauceObject, _id: sauceId })
+          .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -9,5 +9,6 @@ const sauceCtrl = require("../controllers/sauce");
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.get("/", auth, sauceCtrl.getAllSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 
 module.exports = router;
